12cddd59f8b8d839baf0eba8afffe0f0780103f4
CodeBurner <codeburner0@gmail.com>
2022-12-27 11:13:35 +0530

    new question added

--- InterviewQuestions/SortingNames.java
+++ InterviewQuestions/SortingNames.java
@@ -1,7 +1,31 @@
 package InterviewQuestions;
 
+import java.util.Scanner;
+
 public class SortingNames {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        String arr[] = new String[N];
+        // taking input
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].compareTo(arr[j]) > 0) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (String e : arr) {
+            System.out.print(e + " ");
+        }
+
     }
 }
+
+// Input:- Banana Grapes Apple
+// Output:- Apple Banana Apple
